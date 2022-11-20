513147edde583a4d74e7762932379e5e719965b2
Ache2503 <101962554+Ache2503@users.noreply.github.com>
2022-11-19 22:47:48 -0600

    Add files via upload

---
+++ resta.py
@@ -0,0 +1,5 @@
+print(" ***** Resta basica de 2 numeros *****")
+
+x = float(input("INGRESE UN PRIMER NUMERO: "))
+y=float(input("INGRESE UN SEGUNDO NUMERO: "))
+z = print(" LA RESTA DE LOS 2 NUMEROS ES: " ,x - y)
